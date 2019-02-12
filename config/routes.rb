56153039ee73088ba75f18ae7f5fae4967ca3567
AlnaslGamma02 <Alnasl.Gamma02@gmail.com>
2019-02-12 15:07:12 +0900

    Finish(?) static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+
+  root 'welcome#index'
+
   get 'welcome/index'
+
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
 
   resources :articles do
     resources :comments
   end
 
-  root 'welcome#index'
 end
