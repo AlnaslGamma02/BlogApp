f87971e79559f80b09beb684ab488747aff5237e
AlnaslGamma02 <Alnasl.Gamma02@gmail.com>
2019-02-18 11:53:28 +0900

    Adjust the devise

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
+
   def index
     @articles = Article.all
   end
